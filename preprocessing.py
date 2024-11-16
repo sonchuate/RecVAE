d748e9eace4558939f3e8cc6f1445130dea2a761
Tran Cao Son 20215472 <caosonth2003@gmail.com>
2024-11-16 22:25:39 +0700

    fix preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -38,13 +38,13 @@ def get_count(tp, id):
 
 
 def filter_triplets(tp, min_uc=min_uc, min_sc=min_sc): 
-    if min_sc > 0:
-        itemcount = get_count(tp, 'movieId')
-        tp = tp[tp['movieId'].isin(itemcount.index[itemcount >= min_sc])]
+    # if min_sc > 0:
+    #     itemcount = get_count(tp, 'movieId')
+    #     tp = tp[tp['movieId'].isin(itemcount.index[itemcount >= min_sc])]
     
-    if min_uc > 0:
-        usercount = get_count(tp, 'userId')
-        tp = tp[tp['userId'].isin(usercount.index[usercount >= min_uc])]
+    # if min_uc > 0:
+    #     usercount = get_count(tp, 'userId')
+    #     tp = tp[tp['userId'].isin(usercount.index[usercount >= min_uc])]
     
     usercount, itemcount = get_count(tp, 'userId'), get_count(tp, 'movieId') 
     return tp, usercount, itemcount
